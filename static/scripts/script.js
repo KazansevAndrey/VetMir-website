2a62f9efbc903f40e8a1afbec4f1d6f36ed8641e
XXXANdreyYXXX <andrey.kazantsev.04@inbox.ru>
2024-04-30 20:20:29 +0500

    footer1

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,18 +1,5 @@
 
-function openNewWindow(event) {
-    
-    event.preventDefault();
-
-    
-    var newText = "Здесь вы можете оставить отзыв";
-    window.open().document.write(newText);
+function open_menu(){
+    var header = document.getElementById("header");
+    header.classList.toggle("open")
 }
-
-
-var openWindowLink = document.getElementById('openWindowLink');
-
-console.log(openWindowLink)
-
-
-
-openWindowLink.addEventListener('click', openNewWindow);
