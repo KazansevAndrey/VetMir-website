bc5222d17b1ecaaa9e02d6b47b4c652262c6039a
Андрей <andrey.kazantsev.04@inbox.ru>
2024-05-08 22:31:11 +0500

    employees

--- employees/swiper.js
+++ employees/swiper.js
@@ -1,11 +1,11 @@
 var swiper = new Swiper(".slide-content", {
         slidesPerView: 3,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
-        loop: false,
-        loopFillGroupWithBlank: true,
-        centerSlide: true,
-        grabCursor: true,
+        spaceBetween: 25,
+       
+        loop: true,
+        centerSlide: "true",
+        fade: "true",
+        gragCursor: "true",
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
@@ -14,4 +14,17 @@ var swiper = new Swiper(".slide-content", {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
         },
+        breakpoints: {
+          0: {
+            slidesPerView: 1
+          },
+          520:{
+              slidesPerView: 2
+          },
+          950:{
+            slidesPerView: 3
+          }
+        }
       });
+
+    
